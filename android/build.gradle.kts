29959a0105ecb8b04e073c23319b21ab4d00467e
=TahaKhelifi1 <khelifitaha17@mail.com>
2025-10-05 13:32:19 +0100

    firebase setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,12 @@
+plugins {
+    //id("com.android.application") version "8.7.3" apply false
+    //id("com.android.library") version "8.7.3" apply false
+    //id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
+    //id("com.google.firebase.crashlytics") version "2.9.7" apply false
+    //id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+}
+
 allprojects {
     repositories {
         google()
